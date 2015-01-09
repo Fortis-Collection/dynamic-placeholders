473394ef5442315ef3c1fc3f28dd0f82827f9b4b
Jason Bert <jason.bert@goblinrockets.com>
2015-01-09 13:16:30 +0000

    Added code for dynamic placeholders

---
+++ Source/DynamicPlaceholders/Pipelines/ExecutePageEditorAction/DynamicReplaceRendering.cs
@@ -0,0 +1,49 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Layouts;
+using Sitecore.Pipelines.ExecutePageEditorAction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicPlaceholders.Pipelines.ExecutePageEditorAction
+{
+	public class DynamicReplaceRendering : ReplaceRendering
+	{
+		protected override RenderingDefinition DoReplaceRendering(RenderingDefinition sourceRendering, Item targetRenderingItem, DeviceDefinition device)
+		{
+			Assert.ArgumentNotNull(sourceRendering, "sourceRendering");
+			Assert.ArgumentNotNull(targetRenderingItem, "targetRenderingItem");
+			Assert.ArgumentNotNull(device, "device");
+			RenderingDefinition definition = new RenderingDefinition
+			{
+				UniqueId = sourceRendering.UniqueId,
+				Cachable = sourceRendering.Cachable,
+				Conditions = sourceRendering.Conditions,
+				Datasource = sourceRendering.Datasource,
+				ItemID = targetRenderingItem.ID.ToString(),
+				MultiVariateTest = sourceRendering.MultiVariateTest,
+				Parameters = sourceRendering.Parameters,
+				Placeholder = sourceRendering.Placeholder,
+				Rules = sourceRendering.Rules,
+				VaryByData = sourceRendering.VaryByData,
+				ClearOnIndexUpdate = sourceRendering.ClearOnIndexUpdate,
+				VaryByDevice = sourceRendering.VaryByDevice,
+				VaryByLogin = sourceRendering.VaryByLogin,
+				VaryByParameters = sourceRendering.VaryByParameters,
+				VaryByQueryString = sourceRendering.VaryByQueryString,
+				VaryByUser = sourceRendering.VaryByUser
+			};
+
+			if (device.Renderings != null)
+			{
+				int index = device.Renderings.IndexOf(sourceRendering);
+				device.Renderings.RemoveAt(index);
+				device.Renderings.Insert(index, definition);
+			}
+			return definition;
+		}
+	}
+}
