380f64b4957f159b907a8157b41ea4789be33efa
Jason Bert <jason.bert@goblinrockets.com>
2015-11-25 18:22:48 +0000

    Moved placeholder creation to base project

---
+++ Source/DynamicPlaceholders/PlaceholdersContext.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Web;
+using System;
+
+namespace DynamicPlaceholders
+{
+	public class PlaceholdersContext
+	{
+		private const string PlaceholdersCacheKey = "DynamicPlaceholders";
+
+		private static List<string> Placeholders
+		{
+			get
+			{
+				var placeholders = (List<string>)HttpContext.Current.Items[PlaceholdersCacheKey];
+
+				if (placeholders == null)
+				{
+					HttpContext.Current.Items[PlaceholdersCacheKey] = placeholders = new List<string>();
+				}
+
+                return placeholders;
+			}
+		}
+
+		public static string Add(string placeholderName, Guid renderingId)
+		{
+			var placeholder = string.Format("{0}_{1}", placeholderName, renderingId);
+			var count = 0;
+
+			if ((count = Placeholders.Count(dp => dp.StartsWith(placeholder))) > 0)
+			{
+				placeholder = string.Format("{0}_{1}", placeholder, count);
+
+				Placeholders.Add(placeholder);
+			}
+
+			return placeholder;
+		}
+	}
+}
